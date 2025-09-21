15b01c83edbde6333d5463220eed3e42e4633115
livya <llivyaspinagaleano@gmail.com>
2025-09-21 12:18:55 -0400

    Add files via upload

---
+++ herois.js.c
@@ -0,0 +1,29 @@
+function calcularRank(vitorias, derrotas) {
+    //la o saldo
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    
+    if (vitorias <= 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo
+let resultado = calcularRank(10, 10);
+console.log(resultado);
